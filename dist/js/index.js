b97c23a3bd1ae9566173cfb4e737d78fbf977081
Michał Kuwik <Michal7074@poczta.fm>
2019-07-13 16:42:51 +0200

    add header.scss file, styled logo-div and menu-hamburger

---
+++ dist/js/index.js
@@ -0,0 +1,10 @@
+// const hamburger_menu = document.querySelector('.header__menu-hamburger-div');
+
+
+
+// hamburger_menu.addEventListener('click', () => {
+//   const lines = document.querySelectorAll('.header__menu-hamburger-div > div');
+//   lines.forEach(line => {
+//     line.classList.toggle('active');
+//   });
+// });
