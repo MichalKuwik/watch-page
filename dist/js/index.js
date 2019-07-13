ad8de30910fb80b4110ff4d8e2d3bd646ae386b7
Michał Kuwik <Michal7074@poczta.fm>
2019-07-13 16:54:57 +0200

    add logic to serve hamburger button, add background to body , this backgorund created using photoshop

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,10 @@
-// const hamburger_menu = document.querySelector('.header__menu-hamburger-div');
+const hamburger_menu = document.querySelector('.header__menu-hamburger-div');
 
 
 
-// hamburger_menu.addEventListener('click', () => {
-//   const lines = document.querySelectorAll('.header__menu-hamburger-div > div');
-//   lines.forEach(line => {
-//     line.classList.toggle('active');
-//   });
-// });
+hamburger_menu.addEventListener('click', () => {
+  const lines = document.querySelectorAll('.header__menu-hamburger-div > div');
+  lines.forEach(line => {
+    line.classList.toggle('active');
+  });
+});
