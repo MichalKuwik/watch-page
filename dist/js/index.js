1ad3cea2ab644042b54e12ce2655a45972038082
Michał Kuwik <Michal7074@poczta.fm>
2019-07-21 16:17:13 +0200

    RWD is complited, add functionality hide menu on scroll down,show menu on sroll top- index.js

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,40 @@
+//Get dom element
 const hamburger_menu = document.querySelector('.header__menu-hamburger-div');
 
+//state of menu flag
+let activeMenu = false;
 
-
+//onclick
 hamburger_menu.addEventListener('click', () => {
   const lines = document.querySelectorAll('.header__menu-hamburger-div > div');
-  lines.forEach(line => {
-    line.classList.toggle('active');
-  });
+  if (!activeMenu) {
+    lines.forEach(line => {
+      line.classList.add('active');
+      activeMenu = true;
+    });
+  } else {
+    lines.forEach(line => {
+      line.classList.remove('active');
+      activeMenu = false;
+    });
+  }
 });
+
+//hie menu on scroll,show menu on scroll top
+let scrollPos = 0;
+const nav = document.querySelector('.header');
+
+function checkPosition() {
+  let windowY = window.scrollY;
+  if (windowY < scrollPos) {
+    // Scrolling UP
+    nav.classList.add('is-visible');
+    nav.classList.remove('is-hidden');
+  } else {
+    // Scrolling DOWN
+    nav.classList.add('is-hidden');
+    nav.classList.remove('is-visible');
+  }
+  scrollPos = windowY;
+}
+window.addEventListener('scroll', checkPosition);
