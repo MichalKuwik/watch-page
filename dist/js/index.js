1333bf82d17878c012003b756cdccc361f4eed3d
Michał Kuwik <Michal7074@poczta.fm>
2019-07-25 22:08:59 +0200

    animate menu ,and alive them in index.js, create new animation in variables.scss file

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,29 +1,51 @@
 //Get dom element
 const hamburger_menu = document.querySelector('.header__menu-hamburger-div');
+const menu = document.querySelector('.header__menu');
+const menu__items = document.querySelectorAll('.header__menu-link');
+
 // menu flag
 let activeMenu = false;
 
-//onclick
-hamburger_menu.addEventListener('click', () => {
+//First function to do menu's actions
+const menuActions = () => {
+
   const lines = document.querySelectorAll('.header__menu-hamburger-div > div');
+
   if (!activeMenu) {
     lines.forEach(line => {
       line.classList.add('active');
       activeMenu = true;
     });
+    menu__items.forEach(item => {
+      item.classList.add('active__link');
+      activeMenu = true;
+    })
+    menu.classList.add('active__menu');
+    activeMenu = true;
   } else {
     lines.forEach(line => {
       line.classList.remove('active');
       activeMenu = false;
     });
+    menu__items.forEach(item => {
+      item.classList.remove('active__link');
+      activeMenu = false;
+    });
+
+    menu.classList.remove('active__menu');
+    activeMenu = false;
   }
-});
+}
+//onclick function menuActions
+hamburger_menu.addEventListener('click', menuActions);
+
+//--------------------------------------------------------------------------------------------//
 
 //hie menu on scroll,show menu on scroll top
 let scrollPos = 0;
 const nav = document.querySelector('.header');
 
-function checkPosition() {
+const checkPosition = () => {
   let windowY = window.scrollY;
   if (windowY < scrollPos) {
     // Scrolling UP
@@ -38,19 +60,27 @@ function checkPosition() {
 }
 window.addEventListener('scroll', checkPosition);
 
+
+
 // //function change img onclick
+//Get DOM elements for change watch's image
 const watch_arr = ['./img/watch-light.png', './img/watch-dark.png', './img/watch.png'];
 const img_div = document.getElementById('main__section2-img');
-
 const dots = document.querySelectorAll('.main__section2-dot');
-dots.forEach((dot, index) => {
-  dot.addEventListener('click', function () {
-    if (index === 0) {
-      img_div.setAttribute('src', watch_arr[0]);
-    } else if (index === 1) {
-      img_div.setAttribute('src', watch_arr[1]);
-    } else {
-      img_div.setAttribute('src', watch_arr[2]);
-    }
+
+function initAllTime(){
+  dots.forEach((dot, index) => {
+    dot.addEventListener('click', function () {
+      if (index === 0) {
+        img_div.setAttribute('src', watch_arr[0]);
+      } else if (index === 1) {
+        img_div.setAttribute('src', watch_arr[1]);
+      } else {
+        img_div.setAttribute('src', watch_arr[2]);
+      }
+    })
   })
-})
+}
+initAllTime(); //call the function
+
+
