c3037b82a0ec89f3caf6c20698d5f2446c7a71f7
Michał Kuwik <Michal7074@poczta.fm>
2019-07-28 15:58:21 +0200

    instal few packages using npm: jquery,browserify,scrollreveal - creacte file bundle.js and connect this file with index.html file, add smothscroll to menu's links and reorganization index.js code using common.js - thank's for browserifry

--- dist/js/index.js
+++ dist/js/index.js
@@ -23,6 +23,7 @@ const menuActions = () => {
       item.addEventListener('click',() => {
         menu.classList.remove('active__menu')
         lines.forEach(line=> line.classList.remove('active'));
+        activeMenu = false;
       })
       activeMenu = true;
     })
@@ -87,4 +88,63 @@ const initAllTime = () => {
 }
 initAllTime(); //call the function
 
+
+const ScrollReveal = require('scrollreveal');
+//section 1
+//left or top
+ScrollReveal().reveal('.main__section1__article1', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'left',
+  reset: false,
+});
+//right or bottom
+ScrollReveal().reveal('.main__section1__article2', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  reset: false,
+});
+//divs section2
+ScrollReveal().reveal('.section2__article-imgDiv', {
+  duration: 2000,
+  scale: 0.8,
+  reset: false,
+  interval: 100
+});
+//section3 div-contact
+ScrollReveal().reveal('.article__item-imgDiv', {
+  duration: 2000,
+  scale: 0.8,
+  reset: false,
+  interval: 100
+});
+//section3 map-contact
+ScrollReveal().reveal('.main__section3__article__map', {
+  duration: 2000,
+  scale: 0.8,
+  reset: false,
+});
+//footer links
+ScrollReveal().reveal('.footer__link', {
+  duration: 1000,
+  scale: 0.5,
+  reset: false,
+  interval: 100
+});
+
+const $ = require("jquery");
+//smooth Onscroll function
+
+$('a[href*="#"]').on('click', function(e) {
+  e.preventDefault()
+
+  $('html, body').animate(
+      {
+      scrollTop: $($(this).attr('href')).offset().top,
+      },
+      800,
+      'linear'
+  )
+      })
 
